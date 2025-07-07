5dbb15c9b2c46c3ce241a0e6ca5f1c299212fe1a
daniwebdev <dani.webdev@gmail.com>
2025-07-07 21:03:02 +0700

    move laravel auth to modules

--- modules/Authentication/routes/web.php
+++ modules/Authentication/routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+use Modules\Authentication\Http\Controllers\AuthenticatedSessionController;
+use Modules\Authentication\Http\Controllers\ConfirmablePasswordController;
+use Modules\Authentication\Http\Controllers\EmailVerificationNotificationController;
+use Modules\Authentication\Http\Controllers\EmailVerificationPromptController;
+use Modules\Authentication\Http\Controllers\NewPasswordController;
+use Modules\Authentication\Http\Controllers\PasswordResetLinkController;
+use Modules\Authentication\Http\Controllers\RegisteredUserController;
+use Modules\Authentication\Http\Controllers\VerifyEmailController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
