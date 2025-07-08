310bdf947f7c01a89430d04618043e1c928269a3
daniwebdev <dani.webdev@gmail.com>
2025-07-08 08:09:34 +0700

    optimize auth

--- modules/Authentication/app/Http/Controllers/VerifyEmailController.php
+++ modules/Authentication/app/Http/Controllers/VerifyEmailController.php
@@ -12,6 +12,7 @@ class VerifyEmailController extends Controller
     /**
      * Mark the authenticated user's email address as verified.
      */
+    /** @var \Illuminate\Foundation\Auth\EmailVerificationRequest $request */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
